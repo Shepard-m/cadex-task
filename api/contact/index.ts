cb6f211ffc029ea351807a60d4317b5ad5e10ffc
iluvator <petrov34rus@yandex.ru>
2025-05-09 22:52:58 +0300

    fix api

--- api/contact/index.ts
+++ api/contact/index.ts
@@ -1,7 +1,6 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelResponse } from '@vercel/node';
 
 export default async function handler(
-  req: VercelRequest,
   res: VercelResponse
 ) {
   return res.status(200).json({ 
